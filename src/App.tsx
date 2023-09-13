f14cb5db5faa8aa394e654d3df73305af63ea5b7
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-13 23:19:40 +0900

    打ち込んだ情報をコンソールで出力する

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+
+    type Todo = {
+        inputValue: string;
+        id: number;
+        checked: boolean;
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+    }
+    
+
   return (
     <div className="App">
       <div>
         <h2>Todoリスト with Typescript</h2>
         <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText"></input>
+          <input type="text" onChange={(e) => handleChange(e)} className="inputText"></input>
           <input type="submit" value="作成" className="submitButton"></input>
         </form>
       </div>
