de02de22f3883dd7f1c4c34261d917533a98b343
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-13 23:12:13 +0900

    htmlの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">testetset</div>
+    <div className="App">
+      <div>
+        <h2>Todoリスト with Typescript</h2>
+        <form onSubmit={() => {}}>
+          <input type="text" onChange={() => {}} className="inputText"></input>
+          <input type="submit" value="作成" className="submitButton"></input>
+        </form>
+      </div>
+    </div>
   );
 }
 
